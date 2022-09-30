e148a5f8976d69b3cda5b6bd649b826de27bc339
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-09-30 15:24:36 +0300

    Все готово!

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,72 @@ public class Main {
 
         //задача 3
 
+        int year = 2100;
+        if (year%400==0) {
+            System.out.println(year + " год является високосным");
+        } else if (year%100==0) {
+            System.out.println(year + " год не является високосным");
+        } else if (year%4==0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+        //задача 4
+
+        int deliveryDistance = 65;
+        int deliveryDay=1;
+        if (deliveryDistance<=20) {
+            System.out.println("Потребуется дней: " + deliveryDay);
+        } else if (deliveryDistance >20 && deliveryDistance<60) {
+            System.out.println("Потребуется дней: " + (deliveryDay+1));
+        } else if (deliveryDistance>=60 && deliveryDistance<100) {
+            System.out.println("Потребуется дней: " + (deliveryDay+2));
+        }
+
+        //задача 5
+
+        int monthNumber = 5;
+
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Зимний месяц январь");
+                break;
+            case 2:
+                System.out.println("Зимний месяц февраль");
+                break;
+            case 3:
+                System.out.println("Весенний месяц март");
+                break;
+            case 4:
+                System.out.println("Весенний месяц апрель");
+                break;
+            case 5:
+                System.out.println("Весенний месяц май");
+                break;
+            case 6:
+                System.out.println("Летний месяц июнь");
+                break;
+            case 7:
+                System.out.println("Летний месяц июль");
+                break;
+            case 8:
+                System.out.println("Летний месяц август");
+                break;
+            case 9:
+                System.out.println("Осенний месяц сентябрь");
+                break;
+            case 10:
+                System.out.println("Осенний месяц октябрь");
+                break;
+            case 11:
+                System.out.println("Осенний месяц ноябрь");
+                break;
+            case 12:
+                System.out.println("Зимний месяц декабрь");
+                break;
+            default:
+                System.out.println("Некорректные данные");
+        }
     }
 }
