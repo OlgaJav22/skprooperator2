aeea09be4827930e6d60469090ff5d819b2f013a
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-10-01 23:00:09 +0300

    Все д/з доделаны

--- src/Main.java
+++ src/Main.java
@@ -91,5 +91,58 @@ public class Main {
             default:
                 System.out.println("Некорректные данные");
         }
+
+        // задача 6*
+
+        int age = 23;
+        int salary = 88000;
+        int limitMin = salary*2;
+        int limitMax = salary*3;
+
+        if (age>=23 && salary <50000) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + limitMax + " рублей ");
+        } else if (age>=23 && salary >=50000 && salary <80000) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (limitMax*1.2) + " рублей ");
+        } else if (age<23 && salary <50000) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + limitMin + " рублей ");
+        } else if (age <23 && salary >= 50000 && salary <80000) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (limitMin*1.2) + " рублей ");
+        } else if (age>=23 &&salary >=80000) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (limitMax*1.5) + " рублей ");
+        } else if (age <23 && salary >=80000) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (limitMin*1.5) + " рублей ");
+        }
+
+        //ЗАДАЧА 7*
+
+        int age1 = 27;
+        int salary1 = 72_000;
+        int wantedSum = 330_000;
+        double creditPeriod = 12; // срок кредита
+        int paymentMaxMonthly = salary1/2; // ежемесячный платеж 50%
+        double creditPercent = 0.1; // 10% ставка по кредиту
+        double creditPercent1 = 0.005;
+        double creditPercent2 = 0.01;
+        double paymentCredit = wantedSum*((1+creditPercent-0.007)/creditPeriod); // если з/пл больше 80_000 р.
+        double paymentCredit1 = wantedSum*((1+creditPercent+ creditPercent1)/creditPeriod); // если возраст меньше 30 лет
+        double paymentCredit2 = wantedSum*((1+creditPercent + creditPercent2)/creditPeriod); // если возраст меньше 23 лет
+        boolean paymentPossible= paymentMaxMonthly>paymentCredit; // для базовой ставик
+        boolean paymentPossible1= paymentMaxMonthly>paymentCredit1; // верно для возраста меньше 30 лет
+        boolean paymentPossible2= paymentMaxMonthly>paymentCredit2; //верно для возраста меньше 23 лет
+
+        if (salary >80_000 && paymentPossible){
+            System.out.println("Максимальный платеж при з/пл " + salary1 + " равен " +  paymentMaxMonthly + " рублей. Платеж по кредиту " + paymentCredit+ " рублей. Одобрено");
+        } else
+        if (age1<30 && age >= 23 && salary1 <=80_000 && paymentPossible1) {
+            System.out.println("Максимальный платеж при з/пл " + salary1 + " равен " +  paymentMaxMonthly + " рублей. Платеж по кредиту " + paymentCredit1+ " рублей. Одобрено");
+
+        } else  if (age1<23 && salary1 <=80_000 && paymentPossible2){
+            System.out.println("Максимальный платеж при з/пл " + salary1 + " равен " +  paymentMaxMonthly + " рублей. Платеж по кредиту " + paymentCredit2+ " рублей. Одобрено");
+        } else {
+            System.out.println("Вам отказано");
+        }
+
+
     }
+
 }
